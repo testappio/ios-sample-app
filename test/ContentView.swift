42ce3fdc0599fb3a50bc5cf4b9d9bc6b8bfd99bd
Jianbo-Zhu <lemurzjb@gmail.com>
2022-02-24 16:19:58 +0800

    sample ios app

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by 朱建波 on 2022/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
