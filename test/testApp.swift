53d41ce7c8d38a5a3b96dd28a89401dd637299ee
Jianbo-Zhu <lemurzjb@gmail.com>
2022-02-24 23:26:08 +0800

    sample ios app

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 朱建波 on 2022/2/20.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
