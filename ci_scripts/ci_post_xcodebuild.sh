d62bd59d319e606954392fb1e305d4c7a5406362
Omar Masad <3076722+Massad@users.noreply.github.com>
2024-01-04 17:20:39 +0400

    Changing the condition

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,9 +1,12 @@
-if [ "$CI_WORKFLOW" = "TestApp.io Upload" ]; then
+if [ "$CI_PRODUCT_PLATFORM" = "iOS" ]; then
     echo "Uploading to TestApp.io"
+
+    export INSTALL_DIR=$(pwd)
+
     curl -Ls https://github.com/testappio/cli/releases/latest/download/install | bash
 
-    chmod +x ta-cli
+    chmod +x ${INSTALL_DIR}/ta-cli
 
-    ta-cli publish --api_token=$TESTAPPIO_API_TOKEN --app_id=$TESTAPPIO_APP_ID --release="ios" --ipa=$CI_AD_HOC_SIGNED_APP_PATH/sample-app.ipa --release_notes=$release_notes --notify=$notify --source="Xcode Cloud"
+    ${INSTALL_DIR}/ta-cli publish --api_token=$TESTAPPIO_API_TOKEN --app_id=$TESTAPPIO_APP_ID --release="ios" --ipa=$CI_AD_HOC_SIGNED_APP_PATH/sample-app.ipa --release_notes=$release_notes --notify=$notify --source="Xcode Cloud"
 
 fi
