a35d855bc910718bc93c623eb0d33e495b3f69f2
Omar Masad <3076722+Massad@users.noreply.github.com>
2024-01-04 17:09:32 +0400

    adding chmod inside

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,15 +1,8 @@
 if [ "$CI_WORKFLOW" = "TestApp.io Upload" ]; then
     echo "Uploading to TestApp.io"
+    curl -Ls https://github.com/testappio/cli/releases/latest/download/install | bash
 
-    export INSTALL_DIR=$(pwd)
-    if [ ! -f ${INSTALL_DIR}/ta-cli ]; then
-        curl -Ls https://github.com/testappio/cli/releases/latest/download/install | bash
-    else
-        ${INSTALL_DIR}/ta-cli version | grep "You are using the latest version"
-        if [ $? -ne 0 ]; then
-            curl -Ls https://github.com/testappio/cli/releases/latest/download/install | bash
-        fi
-    fi
+    chmod +x ta-cli
 
     ta-cli publish --api_token=$TESTAPPIO_API_TOKEN --app_id=$TESTAPPIO_APP_ID --release="ios" --ipa=$CI_AD_HOC_SIGNED_APP_PATH/sample-app.ipa --release_notes=$release_notes --notify=$notify --source="Xcode Cloud"
 
